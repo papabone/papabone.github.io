7b8dda2c8eb8a3293463ccb4e47ce29eb086821b
Konstantin <papaboneq@gmail.com>
2017-10-04 12:39:07 +0300

    del logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,21 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers'
-import { createLogger } from 'redux-logger'
+/*import { createLogger } from 'redux-logger'*/
 import thunk from 'redux-thunk'
 import { redirect } from '../middlewares/redirect'
 import { routingAnimation } from '../middlewares/routingAnimation'
 import { langAnimation } from '../middlewares/langAnimation'
-import { menu } from '../middlewares/menu' 
+import { menu } from '../middlewares/menu'
 
 
 export default function configureStore(initialState) {
-  const logger = createLogger()
+  //const logger = createLogger()
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     rootReducer,
     initialState,
     composeEnhancers(
-      applyMiddleware(thunk,logger,menu,redirect,routingAnimation,langAnimation )
+      applyMiddleware(thunk/*,logger*/,menu,redirect,routingAnimation,langAnimation )
     )
   )
 
